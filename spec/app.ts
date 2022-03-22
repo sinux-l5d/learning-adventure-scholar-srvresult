e389d1b195a735c0bc5edb2daaa9bfc250ecbee9
JediSim <simpiccc@gmail.com>
2022-03-22 09:32:23 +0100

    doc: ajout test pour service getResultatEtudiants
    Co-authored-by: Paul Delifer <Zall9@users.noreply.github.com>

--- spec/app.ts
+++ spec/app.ts
@@ -1,18 +1,18 @@
-import request from 'supertest';
-import app from '@app';
+import config from '@config';
 
-describe('Path /', () => {
-  // it is an alias for test
-  it('is JSON-encoded', async () => {
-    const res = await request(app).get('/');
+test("L'environnement est en place", () => {
+  expect(config.MONGO_PORT_EXT).toBeDefined();
 
-    expect(res.headers).toHaveProperty('content-type');
-    expect(res.headers['content-type']).toContain('application/json');
-  });
+  // prettier-ignore
+  const env = [
+    'MONGO_APP_USER',
+    'MONGO_APP_PWD',
+    'MONGO_HOST',
+    'MONGO_DB_NAME',
+    'NODE_ENV',
+  ];
 
-  it('contains hello world', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toMatchObject({ message: 'Hello World !' });
+  env.forEach((element) => {
+    expect(config[element]).toBeDefined();
   });
 });
--- src/repositories/resultat.repo.ts
+++ src/repositories/resultat.repo.ts
@@ -2,8 +2,8 @@ import { ExerciceEtudiantModel } from '@db/exercice.db';
 import { ExerciceEtudiant } from '@type/ExerciceEtudiant';
 
 /**
- * Get all the results of the students for the given exercise.
- * @returns {Promise<ExerciceEtudiant[]>} - A promise that resolves to an array of the results of the students for the given exercise.
+ * Recupere tous les resultats des etudiants
+ * @returns {Promise<ExerciceEtudiant[]>} - Une promesse qui renvoie un tableau de resultats d'étudiants.
  * @Todo : Creer le AppError pour ce repo
  */
 export const getResultatsEtudiants = async (): Promise<ExerciceEtudiant[]> => {
